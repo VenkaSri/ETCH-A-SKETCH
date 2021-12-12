842fe37da14b22ef1a99c572d9d3cf56e6fc20c2
Venka <venkasritharan@gmail.com>
2021-12-12 15:10:48 -0500

    new branch

--- scripts.js
+++ scripts.js
@@ -3,12 +3,11 @@ const customGridContainer = document.querySelector('.outer-box');
 
 const linesColor = document.querySelector('#lines');
 const bgColor = document.querySelector('#bgColor');
-let someBgColor = '#eee';
-let someLinesColor = '#eee';
 
-customizeGrid();
 
-/*
+
+
+
 
 
 let num = prompt('what Number');;
@@ -31,7 +30,7 @@ function AppendElements(num) {
 
 function resizableDivs() {
     let divI = document.createElement('div');
-    divI.classList.add('box');
+    divI.classList.add('mainGrid');
     div.append(divI);
     div.style.setProperty('--repeatNumber', num);  
     div.style.gridTemplateColumns = `repeat(${num}, ${700 / num}px)`;  
@@ -39,44 +38,11 @@ function resizableDivs() {
 }
 
 
-*/
+
 
   
-linesColor.addEventListener('input', function(e) {
-    let box = document.querySelectorAll('.box');
-    for (i = 0; i < box.length; ++i) {
-        box[i].style.borderColor = this.value;
-    }
-});
 
 
-
-function customizeGrid() {
-    
-    for (let i = 0; i < 6; i++) {
-        const div = document.createElement('div');
-        customGridContainer.append(div);
-        div.classList.add('box');        
-    }
-
-    
-} 
-let divs = document.querySelectorAll('.box');
-divs.forEach((hoverableElement) => {
-    bgColor.addEventListener('input', function(e) {
-        someBgColor = this.value;
-        hoverableElement.addEventListener('mouseover', (e) => {
-            hoverableElement.style.backgroundColor = this.value;
-            
-            console.log(someBgColor);
-        }); 
-    });
-});
-
-    
-document.querySelector('.p').addEventListener('click', (e) => {
-    document.querySelector('.p').style.color = someBgColor;
-});
 
 
 
