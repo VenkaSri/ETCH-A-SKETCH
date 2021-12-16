f931de30a612d7fc11529db3b18303ae2e390ea8
Venka <venkasritharan@gmail.com>
2021-12-15 21:26:37 -0500

    fixed issue where appendE does not remove previous grid

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const DEFAULT_MO_COLOR = "#000000";
 const DEFAULT_GRID_SIZE = 16;
 
 const container = document.querySelector(".main-container");
+
 const customGridContainer = document.querySelector(".outer-box");
 const applyBtn = document.querySelector(".apply-button");
 const resetBtn = document.querySelector(".reset-button");
@@ -36,8 +37,7 @@ slider.oninput = function() {
 
 
 customizeGrid();
-changeBgColor();
-changeLineColor();
+
 
 function AppendElements(num) {
   const div = document.createElement("div");
@@ -48,43 +48,18 @@ function AppendElements(num) {
     let divI = document.createElement("div");
     divI.classList.add("mainGrid");
     div.append(divI);
-    div.style.setProperty("--repeatNumber", num);
     div.style.gridTemplateColumns = `repeat(${num}, ${700 / num}px)`;
     div.style.gridTemplateRows = `repeat(${num}, ${620 / num}px)`;
   }
 }
-function customizeGrid() {
-  for (let i = 0; i < 6; i++) {
-    const div = document.createElement("div");
-    customGridContainer.append(div);
-    div.classList.add("box");
+
+
+function removeElements() {
+    let gC = document.querySelector(".gridContainer");
+    gC.remove();
   }
-}
-
-linesColor.addEventListener("input", function (e) {
-  let box = document.querySelectorAll(".box");
-  for (i = 0; i < box.length; ++i) {
-    box[i].style.borderColor = this.value;
-    someLinesColor = this.value;
-    lineClr = this.value;
-  }
-});
-
-let bDivs = document.querySelectorAll(".box");
-bDivs.forEach((hoverableElement) => {
-  bgColor.addEventListener("input", function (e) {
-    someBgColor = this.value;
-
-    hoverableElement.addEventListener("mouseover", (e) => {
-      hoverableElement.style.backgroundColor = someBgColor;
-
-      console.log(someBgColor);
-    });
-  });
-});
 
 applyBtn.addEventListener("click", (e) => {
-  checkMatch();
   bgClr = someBgColor;
   AppendElements(gridSize);
   let mDivs = document.querySelectorAll(".mainGrid");
@@ -98,44 +73,6 @@ applyBtn.addEventListener("click", (e) => {
   });
 });
 
-function changeBgColor() {
-  mouserOverMainGrid(DEFAULT_MO_COLOR);
-  mouserOverCustomizeableGrid(DEFAULT_MO_COLOR);
-}
-
-function changeLineColor() {
-  let mDivs = document.querySelectorAll(".mainGrid");
-  for (i = 0; i < mDivs.length; ++i) {
-    mDivs[i].style.borderColor = DEFAULT_LINES_COLOR;
-  }
-}
-
-function mouserOverMainGrid(color) {
-  let mDivs = document.querySelectorAll(".mainGrid");
-  mDivs.forEach((hoverableElement) => {
-    hoverableElement.addEventListener("mouseover", (e) => {
-      hoverableElement.style.backgroundColor = color;
-    });
-  });
-}
-
-function mouserOverCustomizeableGrid(color) {
-  let mDivs = document.querySelectorAll(".box");
-  mDivs.forEach((hoverableElement) => {
-    hoverableElement.addEventListener("mouseover", (e) => {
-      hoverableElement.style.backgroundColor = color;
-    });
-  });
-}
-
-function checkMatch() {
-  if (someBgColor !== bgClr) {
-    let mDivs = document.querySelectorAll(".mainGrid");
-    for (i = 0; i < mDivs.length; ++i) {
-      mDivs[i].style.backgroundColor = "#ffffff";
-    }
-  }
-}
 
 
 function mainGridBg(color) {
@@ -152,13 +89,21 @@ function mainGridLine(color) {
   }
 }
 
+function customizeGrid() {
+    for (let i = 0; i < 6; i++) {
+      const div = document.createElement("div");
+      customGridContainer.append(div);
+      div.classList.add("box");
+    }
+  }
+
 resetBtn.addEventListener('click', () => {
   mainGridBg(DEFAULT_BG_COLOR);
   mainGridLine(DEFAULT_LINES_COLOR);
 });
 
 eraserBtn.addEventListener('click', () => {
-  mouserOverMainGrid(DEFAULT_BG_COLOR);
-  mouserOverCustomizeableGrid("white");
+
+  removeElements();
 });
 
